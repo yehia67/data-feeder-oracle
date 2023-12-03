2d30e01d8a75f4c611cea2b02fe4fac8f6556fba
Yehia Tarek <yehia@startfi.io>
2023-12-03 10:21:14 +0200

    chor: change the port number

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new AllExceptionsFilter());
 
-  await app.listen(3000);
+  await app.listen(4000);
 }
+
 bootstrap();
