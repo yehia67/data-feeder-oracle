396e10c3c08178e1fd1a2cc8d2a5a22df41afa53
Yehia Tarek <yehiatarek67@gmail.com>
2023-12-09 19:11:49 +0200

    Refactor code to use event listener instead of polling the RPC node  (#10)
    * feat: use node 20 instead of noe 18
    
    * feat: add dockerfile
    
    * refact: use subscribe event instead of logging

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { AllExceptionsFilter } from "filters/http-exception.filter";
+
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 require("dotenv").config();
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new AllExceptionsFilter());
 
-  await app.listen(4000);
+  await app.listen(process.env.APP_PORT);
 }
 
 bootstrap();
